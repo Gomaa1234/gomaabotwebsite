c0580ad9b9e94ed9a7ad7ad516f83ff049cf7da8
Gonçalo Alegria <goncaloantonioalegria16@gmail.com>
2023-07-02 23:33:12 +0100

    text add to body

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,12 @@
 const Body=()=>{
     return(
         <div className="Body">
-            <p>Ola</p>
+            <div className=" flex flex-col items-center justify-center "  >
+                <h1 className=" text-4xl font-bold mb-5" >Head Text</h1>
+                <p className=" text-center w-80 leading-relaxed capitalize">Lorem ipsum dolor sit amet, consectetur adipiscing 
+                    elit. Etiam a pharetra ligula, eget lobortis risus. 
+                    Cras eu mi a neque eleifend luctus sagittis posuere nulla.</p>
+            </div>
         </div>
     );
 };
